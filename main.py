8b388e3482d42429062b6a1f9676df8d02142cec
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2013-09-26 00:41:45 +1000

    Completed client-side script.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # globals
 website = "http://xpressen.com/arch-sync/"
+website = "http://localhost:3000/arch-sync/"
 get_pacman_packages = 'pacman -Qqe | grep -vx "$(pacman -Qqm)"'
 get_aur_packages = 'pacman -Qqm'
 
@@ -9,7 +10,7 @@ get_aur_packages = 'pacman -Qqm'
 def main():
     # setup arguements
     parser = argparse.ArgumentParser(description='Sync arch packages between computers.')
-    parser.add_argument("-i", "--id", dest="id", help="Download id found in the url " + website + "/ID")
+    parser.add_argument("-d", "--download", dest="id", help="Download package list found in the url " + website + "/USERNAME")
     args = parser.parse_args()
     if args.id == None:
         # get pacman package list
@@ -20,7 +21,7 @@ def main():
         if(resp == "" or resp == "Y" or resp == "y"):
             # start the upload
             print("Uploading....")
-            values = {'pacman': pacman, 'aur': aur}
+            values = {'pacman': pacman, 'aur': aur, 'username':  os.getlogin()}
             data = urllib.parse.urlencode(values)
             binary_data = data.encode('utf-8')
             req = urllib.request.Request(website, binary_data)
@@ -28,10 +29,32 @@ def main():
             page = res.read().decode('utf-8')
             print(page)
         else:
-            print("Okay. Now attempting to wipe your home dir, Just kidding!")
+            print("Exiting...")
     else:
         # downoad from the specified args.id
-        print("Download!")
+        new_url = website + args.id
+        print("Downloading packages from " + new_url)
+        req = urllib.request.Request(new_url)
+        res = urllib.request.urlopen(req)
+        page = json.loads(res.read().decode('utf-8'))
+        loc = input("Save files to (default current directory): ")
+        if len(loc) > 0:
+          # save to current directory
+          path = os.path.normpath(loc)
+        else:
+          # save to specified directory
+          path = os.getcwd()
+        path += os.sep
+        save_to_file(path + "/Pacman_Packages", page["pacman"])
+        save_to_file(path + "/AUR_Packages", page["aur"])
+
+
+def save_to_file(location, data):
+  print("Saving data to file: " + location)
+  fp = open(location, "w")
+  fp.write(data)
+  fp.close()
+
 
 if __name__ == '__main__':
     try:
@@ -40,8 +63,10 @@ if __name__ == '__main__':
         import argparse # parameter parsing
         import urllib # url utility
         from urllib.request import urlopen # url utility
+        import os # get username
+        import json # decode recieved data
 
-        # did not faile on imports, run program
+        # did not fail on imports, run program
         main()
     except ImportError as error:
         print("Missing python module: " + str(error)[16:])
